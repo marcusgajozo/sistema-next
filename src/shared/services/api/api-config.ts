1eb8065ccda9e10507f57c2739407b3ebdc080b3
Marcus Gajozo <marcusgajozo@gmail.com>
2024-05-10 08:26:54 -0400

    fix: Ajusta configuração do axios

--- src/shared/services/api/api-config.ts
+++ src/shared/services/api/api-config.ts
@@ -1,16 +1,20 @@
 import { getStorageItem } from '@/shared/utils/localStorage';
 import axios from 'axios';
 
-const apiConfig = () => {
-  const tokenLocalStorage = getStorageItem('token')
+const axiosBaseConfig = (token?: string) => {
   return axios.create({
     baseURL: 'https://teste.grupoimagetech.com.br/api/v1',
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
-      Authorization: `Bearer ${tokenLocalStorage}`
+      Authorization: `Bearer ${token}`
     },
   });
 };
 
-export default apiConfig();
+export const apiConfig = () => {
+  const token = getStorageItem('token')
+  return axiosBaseConfig(String(token))
+};
+
+export default apiConfig()
